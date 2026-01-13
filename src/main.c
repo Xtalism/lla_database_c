49ee6ee5dbb084a4e87762b2b76647ce20cacc40
Xtalism <manuel_lego1234@hotmail.com>
2026-01-13 15:57:40 -0700

    Test for parse.c

--- src/main.c
+++ src/main.c
@@ -21,6 +21,7 @@ int main(int argc, char *argv[]) {
     int c;
 
     int dbfd = -1;
+    struct dbheader_t *dbhdr = NULL;
 
     while ((c = getopt(argc, argv, "nf:")) != -1) {
         switch(c) {
@@ -49,10 +50,19 @@ int main(int argc, char *argv[]) {
             printf("Unable to create database file\n");
             return -1;
         }
+        if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to create database header\n");
+            return -1;
+        }
     } else {
         dbfd = open_db_file(filepath);
         if (dbfd == STATUS_ERROR) {
             printf("Unable to open database file\n");
+            return -1;
+        }
+
+        if  (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+            printf("Failed to validate database header\n");
             return -1;
         }
     }
@@ -60,5 +70,7 @@ int main(int argc, char *argv[]) {
     printf("Newfile: %d\n", newfile);
     printf("Filepath: %s\n", filepath);
     
+    output_file(dbfd, dbhdr);
+
     return 0;
 }
